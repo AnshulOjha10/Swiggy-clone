a313ffaf8394019874258f489b5045b573097646
AnshulOjha10 <anshulojha091@gmail.com>
2024-08-05 11:07:18 +0530

    images error fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import swiggylogo from '../assets/swiggy-logo.svg'
 import '../App.css'
 function Footer() {
   return (
         <footer>
             <div>
-            <img className='footerImg' src="./assets/swiggy-logo.svg" alt="photo" />
+            <img className='footerImg' src={swiggylogo} alt="photo" />
             <p> 	&#169;Create By Anshul Ojha</p>
             </div>
         </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import swiggylogo from '../assets/swiggy-logo.svg'
 import "../App.css"
 function Header() {
   return (
     <>
       <div className="header">
-        <img src="./assets/swiggy-logo.svg" alt="photo" />
+        <img src={swiggylogo} alt="photo" />
         <div className="searchItem">
           <input type="search" name="search" id="search" placeholder="Search for restaurant"/>
           <label htmlFor="search"><i className="fa-solid fa-magnifying-glass"></i></label>
